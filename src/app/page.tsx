68a060bbc1ee1b667ad602cf5ae7f0eff88d2df6
KobaBooba <kobayashi_01@outlook.com>
2024-05-15 17:35:12 -0500

    si

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,17 +7,17 @@ export default function Home() {
       <div className="relative items-center justify-between p-24" style={{ backgroundImage: `url('/bg.png')`, height: "968px", }}>
         <div className="absolute inset-0 bg-slate-500 opacity-70" />
         <main className="relative">
-          <div className="px-6">
-            <div className="mx-auto max-w-2xl py-40 sm:py-48 lg:py-56">
-              <div className="text-center">
-                <h1 className="text-4xl sm:text-6x pt-20 font-extrabold">Ley de la conservacion de la Materia</h1>
-                <p className="mt-6 text-lg leading-8 font-extrabold">Santiago Redondo ・ Tomas Casas ・ Paula Gutierrez</p>
-                <div className="mt-10 flex items-center justify-center gap-x-6">
-                </div>
-              </div>
-            </div>
-          </div>
-        </main>
+  <div className="px-6 md:px-0">
+    <div className="mx-auto max-w-2xl py-10 sm:py-20 md:py-40 lg:py-56">
+      <div className="text-center">
+        <h1 className="text-2xl sm:text-4xl md:text-6xl pt-10 sm:pt-20 font-extrabold">Ley de la conservacion de la Materia</h1>
+        <p className="mt-4 sm:mt-6 text-sm sm:text-lg leading-8 font-extrabold">Santiago Redondo ・ Tomas Casas ・ Paula Gutierrez</p>
+        <div className="mt-5 sm:mt-10 flex items-center justify-center gap-x-6">
+        </div>
+      </div>
+    </div>
+  </div>
+</main>
       </div>
     </>
   );
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,21 +1,21 @@
 export default function Header() {
-  return (
+    return (
       <header className="bg-slate-600 bg-opacity">
-          <nav className="mx-auto flex max-w-7xl items-center justify-between p-4 lg:px-6" aria-label="Global">
-              <div className="flex lg:flex-1 justify-center items-center">
-                  <a href="#" className="">
-                      <img className="h-8 w-auto" src="https://cdn-icons-png.flaticon.com/512/3081/3081530.png" alt="Logo"/>
-                  </a>
-              </div>
-              <div className="hidden lg:flex lg:gap-x-12 justify-center items-center">
-                  <a href="/" className="text-sm font-semibold leading-6 ">Inicio</a>
-                  <a href="/infografias" className="text-sm font-semibold leading-6 ">Infografias</a>
-                  <a href="/analisis" className="text-sm font-semibold leading-6 ">Analisis</a>
-                  <a href="/webgrafia" className="text-sm font-semibold leading-6 ">Webgrafia</a>
-              </div>
-              <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-              </div>
-          </nav>
+        <nav className="mx-auto flex flex-wrap max-w-7xl items-center justify-between p-4 lg:px-6" aria-label="Global">
+          <div className="flex w-full lg:w-auto lg:flex-1 justify-center items-center mb-4 lg:mb-0">
+            <a href="#" className="">
+              <img className="h-8 w-auto" src="https://cdn-icons-png.flaticon.com/512/3081/3081530.png" alt="Logo"/>
+            </a>
+          </div>
+          <div className="flex flex-col lg:flex-row lg:gap-x-12 w-full lg:w-auto justify-center items-center">
+            <a href="/" className="text-sm font-semibold leading-6 mb-2 lg:mb-0">Inicio</a>
+            <a href="/infografias" className="text-sm font-semibold leading-6 mb-2 lg:mb-0">Infografias</a>
+            <a href="/analisis" className="text-sm font-semibold leading-6 mb-2 lg:mb-0">Analisis</a>
+            <a href="/webgrafia" className="text-sm font-semibold leading-6 mb-2 lg:mb-0">Webgrafia</a>
+          </div>
+          <div className="hidden lg:flex lg:flex-1 lg:justify-end">
+          </div>
+        </nav>
       </header>
-  )
-}
+    );
+  }
