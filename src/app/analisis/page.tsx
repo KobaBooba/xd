62c3ddc3a2d5b6367ef63006a8e37ae2cbd1c948
KobaBooba <kobayashi_01@outlook.com>
2024-05-15 17:55:28 -0500

    cvsdfvws

--- src/app/analisis/page.tsx
+++ src/app/analisis/page.tsx
@@ -24,26 +24,25 @@ export default function HomePage() {
             <div className="absolute inset-0 flex flex-col items-center justify-center"> {/* Modified this line */}
   <div className="flex flex-col items-center justify-center w-full p-8"> {/* Modified this line */}
     <h1 className="text-2xl font-bold mb-2">Análisis</h1> {/* Added this line */}
-    <iframe 
-      width="560" 
-      height="315" 
-      src="https://www.youtube.com/embed/f9bix7MlQhg" 
-      title="YouTube video player" 
-      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
-    ></iframe>
-  </div>
-  <div className="flex flex-col items-center justify-center w-full p-8"> {/* Modified this line */}
-    <h1 className="text-2xl  font-extrabold">¿Cómo se hacen los vidrios?</h1> {/* Moved this line */}
+    <iframe
+  src="https://www.youtube.com/embed/f9bix7MlQhg" 
+  title="YouTube video player" 
+  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+></iframe>
+</div>
+<div className="md:mt-0 mt-20"> {/* Aumenta el margen en la parte superior solo en dispositivos móviles */}
+  <div className="flex flex-col items-center justify-center">
     <img 
       src="/1.png" 
       alt="" 
-      className="w-48 h-auto cursor-pointer border-2 border-gray-300" /* Modified this line */
+      className="w-48 h-auto cursor-pointer border-2 border-gray-300 md:w-auto" /* Ajusta el ancho solo en dispositivos móviles */
       onClick={onOpenModal}
     />
     <Modal open={isOpen} onClose={onCloseModal} center>
       <img src="/1.png" alt="" className="w-full max-h-full object-contain" />
     </Modal>
   </div>
+</div>
 </div>
           </div>
         </div>
