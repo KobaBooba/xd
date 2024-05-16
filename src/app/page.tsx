0651500c92337b973822a19f1a561ac3aa253766
Koba <90475380+KobaBooba@users.noreply.github.com>
2024-05-16 08:39:02 -0500

    Actualizar page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
         <div className="absolute inset-0 bg-slate-500 opacity-70" />
         <main className="relative">
   <div className="px-6 md:px-0">
-    <div className="mx-auto max-w-2xl py-10 sm:py-20 md:py-40 lg:py-56">
+    <div className="mx-auto max-w-2xl py-10 sm:py-20 md:py-40 lg:py-56 text-white">
       <div className="text-center">
         <h1 className="text-2xl sm:text-4xl md:text-6xl pt-10 sm:pt-20 font-extrabold">Ley de la conservación de la Materia</h1>
         <p className="mt-4 sm:mt-6 text-sm sm:text-lg leading-8 font-extrabold">Santiago Redondo ・ Tomas Casas ・ Paula Gutierrez</p>
