779e0f2494257461afdffb1293b3338c09873b14
Koba <90475380+KobaBooba@users.noreply.github.com>
2024-05-16 08:39:35 -0500

    Actualizar page.tsx

--- src/app/webgrafia/page.tsx
+++ src/app/webgrafia/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
         <div className="absolute inset-0 bg-slate-500 opacity-70 flex flex-col items-center justify-center">
           <main className="flex flex-col items-center justify-center p-6 h-screen w-screen">
             <div className="bg-black p-6 text-center">
-              <div className="p-6">
+              <div className="p-6 text-white">
                 <h1 className="text-4xl sm:text-6x font-extrabold">Webgrafía</h1>
                 <h1 className ="py-6 font-extrabold"> <a href="https://pea.ucr.ac.cr/wp-content/uploads/2020/08/Balanceo-por-Tanteo.pdf">Ley de la conservación de la Materia</a></h1>
               
